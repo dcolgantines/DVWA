fbb1098506772d91aaba178750c50c9bcf78af44
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2023-11-01 15:41:02 +0100

    HTTP request redirections should not be open to forging attacks

---
+++ model/products.js
@@ -0,0 +1,17 @@
+<?php
+
+if (array_key_exists ("redirect", $_GET) && $_GET['redirect'] != "") {
+```php
+if (isset($_GET['redirect'])) {
+    $redirect = filter_var($_GET['redirect'], FILTER_SANITIZE_URL);
+    header("Location: " . $redirect);
+    exit;
+}
+
+http_response_code(500);
+?>
+<p>Missing redirect target.</p>
+<?php
+exit;
+?>
+```
