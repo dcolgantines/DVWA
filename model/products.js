096fcb74e03960be191414b31536dcb1b340762d
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2023-11-01 15:41:32 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,103 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] = 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+
+if (array_key_exists ("id", $_GET)) {
+	$id = $_GET[ 'id' ];
+
+	$lowsrc = @file_get_contents("./{$id}/source/low.php");
+To remediate the security vulnerability and avoid constructing the path from user-controlled data, you can use an associative array to map the vulnerability IDs to their corresponding names. Here's the updated code:
+
+```php
+$vulnerabilityNames = array(
+    "javascript" => 'JavaScript',
+    "fi" => 'File Inclusion',
+    "brute" => 'Brute Force',
+    "csrf" => 'CSRF',
+    "exec" => 'Command Injection',
+    "sqli" => 'SQL Injection',
+    "sqli_blind" => 'SQL Injection (Blind)',
+    "upload" => 'File Upload',
+    "xss_r" => 'Reflected XSS',
+    "xss_s" => 'Stored XSS',
+    "weak_id" => 'Weak Session IDs',
+    "authbypass" => 'Authorisation Bypass',
+    "open_redirect" => 'Open HTTP Redirect'
+);
+
+$vuln = isset($vulnerabilityNames[$id]) ? $vulnerabilityNames[$id] : "Unknown Vulnerability";
+
+$lowsrc = str_replace('$html .=', 'echo', $lowsrc);
+$lowsrc = highlight_string($lowsrc, true);
+
+$medsrc = @file_get_contents("./{$id}/source/medium.php");
+$medsrc = str_replace('$html .=', 'echo', $medsrc);
+$medsrc = highlight_string($medsrc, true);
+
+$highsrc = @file_get_contents("./{$id}/source/high.php");
+$highsrc = str_replace('$html .=', 'echo', $highsrc);
+$highsrc = highlight_string($highsrc, true);
+
+$impsrc = @file_get_contents("./{$id}/source/impossible.php");
+$impsrc = str_replace('$html .=', 'echo', $impsrc);
+$impsrc = highlight_string($impsrc, true);
+
+$page['body'] .= "
+<div class=\"body_padded\">
+    <h1>{$vuln}</h1>
+";
+```
+
+By using the associative array `$vulnerabilityNames`, we can map the vulnerability IDs to their names and avoid directly using user-controlled data to construct the vulnerability name. This prevents any manipulation of the path and ensures the code is more secure.
+		<br />
+
+		<h3>Impossible {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$impsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>High {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$highsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Medium {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$medsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Low {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$lowsrc}</div></td>
+			</tr>
+		</table>
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"<-- Back\" onclick=\"history.go(-1);return true;\">
+		</form>
+
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho( $page );
+
+?>
