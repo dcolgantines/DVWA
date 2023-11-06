54999f3f45d29c8733c4419ae185d45c5c1f2085
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2023-11-06 10:01:54 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,123 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] .= 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+
+if (array_key_exists ("id", $_GET) && array_key_exists ("security", $_GET)) {
+	$id       = $_GET[ 'id' ];
+	$security = $_GET[ 'security' ];
+
+
+	switch ($id) {
+		case "fi" :
+			$vuln = 'File Inclusion';
+			break;
+		case "brute" :
+			$vuln = 'Brute Force';
+			break;
+		case "csrf" :
+			$vuln = 'CSRF';
+			break;
+		case "exec" :
+			$vuln = 'Command Injection';
+			break;
+		case "sqli" :
+			$vuln = 'SQL Injection';
+			break;
+		case "sqli_blind" :
+			$vuln = 'SQL Injection (Blind)';
+			break;
+		case "upload" :
+			$vuln = 'File Upload';
+			break;
+		case "xss_r" :
+			$vuln = 'Reflected XSS';
+			break;
+		case "xss_s" :
+			$vuln = 'Stored XSS';
+			break;
+		case "weak_id" :
+			$vuln = 'Weak Session IDs';
+			break;
+		case "javascript" :
+			$vuln = 'JavaScript';
+To fix the security vulnerability of constructing the path from user-controlled data, you should sanitize and validate the input before using it. One way to achieve this is by creating a whitelist of allowed values for the `$security` variable and checking if the user-provided value exists in the whitelist. If it does, then you can safely construct the path. Here's the updated code:
+
+```php
+<?php
+// Create a whitelist of allowed values for the $security variable
+$allowedSecurityValues = ['command_injection', 'authbypass', 'open_redirect'];
+
+// Sanitize and validate the input
+$security = isset($_GET['security']) ? $_GET['security'] : '';
+if (!in_array($security, $allowedSecurityValues)) {
+    $security = 'unknown';
+}
+
+// Handle the case based on the sanitized value
+switch ($security) {
+    case "command_injection":
+        $vuln = 'Command Injection';
+        break;
+    case "authbypass":
+        $vuln = 'Authorisation Bypass';
+        break;
+    case "open_redirect":
+        $vuln = 'Open HTTP Redirect';
+        break;
+    default:
+        $vuln = "Unknown Vulnerability";
+}
+
+$source = @file_get_contents( DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.php" );
+$source = str_replace( array( '$html .=' ), array( 'echo' ), $source );
+
+$js_html = "";
+if (file_exists (DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.js")) {
+    $js_source = @file_get_contents( DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.js" );
+    $js_html = "
+		<h2>vulnerabilities/{$id}/source/{$security}.js</h2>
+		<div id=\"code\">
+			<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+				<tr>
+					<td><div id=\"code\">" . highlight_string( $js_source, true ) . "</div></td>
+				</tr>
+			</table>
+		</div>
+		";
+}
+
+$page[ 'body' ] .= "
+	<div class=\"body_padded\">
+		<h1>{$vuln} Source</h1>
+
+		<h2>vulnerabilities/{$id}/source/{$security}.php</h2>
+		<div id=\"code\">
+			<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+				<tr>
+					<td><div id=\"code\">" . highlight_string( $source, true ) . "</div></td>
+				</tr>
+			</table>
+		</div>
+		{$js_html}
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"Compare All Levels\" onclick=\"window.location.href='view_source_all.php?id=$id'\">
+		</form>
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho( $page );
+
+?>
+```
+
+By implementing this change, you prevent the user from providing arbitrary values for the `$security` variable and restrict it to specific whitelisted values.
