2400f7ad1aa2ebca7f8ce6ce3c6ccf432032080e
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2023-11-13 10:02:03 +0100

    OS commands should not be vulnerable to command injection attacks

---
+++ model/products.js
@@ -0,0 +1,29 @@
+<?php
+```
+if( isset( $_POST[ 'Submit' ] ) ) {
+	// Get input
+	$target = $_REQUEST[ 'ip' ];
+
+	// Make sure the target IP is valid
+	if ( !filter_var( $target, FILTER_VALIDATE_IP ) ) {
+		$html .= "<pre>Invalid IP address</pre>";
+	}
+
+	// Determine OS and execute the ping command.
+	if ( stristr( php_uname( 's' ), 'Windows NT' ) ) {
+		// Windows
+		$cmd = shell_exec( 'ping  ' . escapeshellarg( $target ) );
+	} else {
+		// *nix
+		$cmd = shell_exec( 'ping  -c 4 ' . escapeshellarg( $target ) );
+	}
+
+	// Feedback for the end user
+	$html .= "<pre>{$cmd}</pre>";
+}
+```
+Explanation: 
+
+Added input validation to ensure the target IP address is valid using the `FILTER_VALIDATE_IP` filter. This prevents using potentially malicious or invalid IP addresses in the command.
+
+Used `escapeshellarg` function to safely escape the target IP address before concatenating it into the shell command. This prevents shell command injection by properly handling special characters and preventing the injection of additional commands.
