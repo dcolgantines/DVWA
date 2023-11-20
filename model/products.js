34a5d373fea274244a6f496c6a94a72fc5252511
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2023-11-20 10:02:38 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,113 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] = 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+To avoid constructing the path from user-controlled data, you can create a whitelist of allowed IDs and use that to retrieve the corresponding file contents. Here's the modified code:
+
+```php
+$allowedIds = array(
+    "javascript",
+    "fi",
+    "brute",
+    "csrf",
+    "exec",
+    "sqli",
+    "sqli_blind",
+    "upload",
+    "xss_r",
+    "xss_s",
+    "weak_id",
+    "authbypass"
+);
+
+if (array_key_exists("id", $_GET)) {
+    $id = $_GET['id'];
+
+    if (in_array($id, $allowedIds)) {
+        $lowsrc = file_get_contents("./{$id}/source/low.php");
+        $lowsrc = str_replace(array('$html .='), array('echo'), $lowsrc);
+        $lowsrc = highlight_string($lowsrc, true);
+
+        $medsrc = file_get_contents("./{$id}/source/medium.php");
+        $medsrc = str_replace(array('$html .='), array('echo'), $medsrc);
+        $medsrc = highlight_string($medsrc, true);
+
+        $highsrc = file_get_contents("./{$id}/source/high.php");
+        $highsrc = str_replace(array('$html .='), array('echo'), $highsrc);
+        $highsrc = highlight_string($highsrc, true);
+
+        $impsrc = file_get_contents("./{$id}/source/impossible.php");
+        $impsrc = str_replace(array('$html .='), array('echo'), $impsrc);
+        $impsrc = highlight_string($impsrc, true);
+
+        // Rest of the code...
+    } else {
+        // Handle invalid ID error
+        echo "Invalid ID";
+        exit;
+    }
+}
+```
+
+In this code, we create an array `$allowedIds` that contains the whitelisted IDs. We then check if the user-provided `$id` is present in the whitelist using the `in_array` function. If it is, we proceed to fetch the file contents as before. If it's not in the whitelist, we handle it as an invalid ID and display an appropriate error message.
+		case "open_redirect" :
+			$vuln = 'Open HTTP Redirect';
+			break;
+		default:
+			$vuln = "Unknown Vulnerability";
+	}
+
+	$page[ 'body' ] .= "
+	<div class=\"body_padded\">
+		<h1>{$vuln}</h1>
+		<br />
+
+		<h3>Impossible {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$impsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>High {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$highsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Medium {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$medsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Low {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$lowsrc}</div></td>
+			</tr>
+		</table>
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"<-- Back\" onclick=\"history.go(-1);return true;\">
+		</form>
+
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho( $page );
+
+?>
