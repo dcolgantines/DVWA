26d0f541c6865629f4a759bf4996cda04bffc518
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2023-11-20 10:01:16 +0100

    Endpoints should not be vulnerable to reflected cross-site scripting (XSS) attacks

---
+++ model/products.js
@@ -0,0 +1,17 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+if (array_key_exists ("callback", $_GET)) {
+	$callback = $_GET['callback'];
+} else {
+	return "";
+}
+
+$outp = array ("answer" => "15");
+
+echo $callback . "(".json_encode($outp).")";
+```php
+// The original snippet was already forbidden, however, this is a version without
+// any echo of user-controlled data.
+```
+```
