87294349d5694232e2cb09b5a053962ae2562c7b
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2023-12-04 10:01:25 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,95 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] = 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+To avoid constructing the path from user-controlled data, you can create an associative array to map the `$id` value to the corresponding file path and vulnerability type. Here's the updated code:
+
+```php
+if (array_key_exists("id", $_GET)) {
+    $id = $_GET['id'];
+
+    $vulnerabilities = [
+        "javascript" => ['source/low.php', 'JavaScript'],
+        "fi" => ['source/medium.php', 'File Inclusion'],
+        "brute" => ['source/high.php', 'Brute Force'],
+        "csrf" => ['source/impossible.php', 'CSRF'],
+        // Add other vulnerabilities mappings here...
+    ];
+
+    if (isset($vulnerabilities[$id])) {
+        $file = $vulnerabilities[$id][0];
+        $vuln = $vulnerabilities[$id][1];
+
+        $content = @file_get_contents($file);
+        $content = str_replace(array('$html .='), array('echo'), $content);
+        $content = highlight_string($content, true);
+
+        // Rest of your code...
+    } else {
+        // Handle invalid or unknown id
+    }
+}
+```
+
+By using the `$vulnerabilities` array, you can safely map the `$id` value to the corresponding file path and vulnerability type without directly including user-controlled data in the file path. This helps prevent directory traversal or arbitrary file access vulnerabilities.
+		case "open_redirect" :
+			$vuln = 'Open HTTP Redirect';
+			break;
+		default:
+			$vuln = "Unknown Vulnerability";
+	}
+
+	$page[ 'body' ] .= "
+	<div class=\"body_padded\">
+		<h1>{$vuln}</h1>
+		<br />
+
+		<h3>Impossible {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$impsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>High {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$highsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Medium {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$medsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Low {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$lowsrc}</div></td>
+			</tr>
+		</table>
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"<-- Back\" onclick=\"history.go(-1);return true;\">
+		</form>
+
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho( $page );
+
+?>
