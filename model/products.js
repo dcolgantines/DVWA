f2a6b5fe5d0394bf7ea5bbc8ea9ac9a627588c99
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2023-12-11 10:01:31 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,113 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] = 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+To fix the security vulnerability in this code, you need to remove the direct construction of the file path using user-controlled data. Instead, you can use an associative array or a switch statement to map the user input to the corresponding file path.
+
+Here's the updated code:
+
+```php
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $vuln = '';
+    $filePaths = [
+        'javascript' => 'javascript/source',
+        'fi' => 'fi/source',
+        'brute' => 'brute/source',
+        'csrf' => 'csrf/source',
+        'exec' => 'exec/source',
+        'sqli' => 'sqli/source',
+        'sqli_blind' => 'sqli_blind/source',
+        'upload' => 'upload/source',
+        'xss_r' => 'xss_r/source',
+        'xss_s' => 'xss_s/source',
+        'weak_id' => 'weak_id/source',
+        'authbypass' => 'authbypass/source',
+    ];
+
+    if (isset($filePaths[$id])) {
+        $filePath = $filePaths[$id];
+        $lowsrc = @file_get_contents("./{$filePath}/low.php");
+        $lowsrc = str_replace(array('$html .='), array('echo'), $lowsrc);
+        $lowsrc = highlight_string($lowsrc, true);
+
+        $medsrc = @file_get_contents("./{$filePath}/medium.php");
+        $medsrc = str_replace(array('$html .='), array('echo'), $medsrc);
+        $medsrc = highlight_string($medsrc, true);
+
+        $highsrc = @file_get_contents("./{$filePath}/high.php");
+        $highsrc = str_replace(array('$html .='), array('echo'), $highsrc);
+        $highsrc = highlight_string($highsrc, true);
+
+        $impsrc = @file_get_contents("./{$filePath}/impossible.php");
+        $impsrc = str_replace(array('$html .='), array('echo'), $impsrc);
+        $impsrc = highlight_string($impsrc, true);
+
+        $vuln = ucfirst($id);
+    }
+}
+```
+
+In this code, we use the `$filePaths` array to map the user input to the corresponding file path. If the user input is valid and exists in the array, we construct the file paths based on the mapped value from the array. This ensures that the file path is not controlled by the user directly, preventing directory traversal or path manipulation attacks.
+		case "open_redirect" :
+			$vuln = 'Open HTTP Redirect';
+			break;
+		default:
+			$vuln = "Unknown Vulnerability";
+	}
+
+	$page[ 'body' ] .= "
+	<div class=\"body_padded\">
+		<h1>{$vuln}</h1>
+		<br />
+
+		<h3>Impossible {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$impsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>High {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$highsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Medium {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$medsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Low {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$lowsrc}</div></td>
+			</tr>
+		</table>
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"<-- Back\" onclick=\"history.go(-1);return true;\">
+		</form>
+
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho( $page );
+
+?>
