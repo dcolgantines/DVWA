e4bb63190eb60b999d4470b340e7d8ddce70f8c2
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2023-12-11 10:01:31 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,55 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+```php
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] = 'Help' . $page[ 'title_separator' ].$page[ 'title' ];
+
+if (array_key_exists ("id", $_GET) &&
+    array_key_exists ("security", $_GET) &&
+    array_key_exists ("locale", $_GET)) {
+    $id       = $_GET[ 'id' ];
+    $security = $_GET[ 'security' ];
+    $locale = $_GET[ 'locale' ];
+
+    ob_start();
+    if ($locale == 'en') {
+        $filePath = DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/help/help.php";
+    } else {
+        $filePath = DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/help/help.{$locale}.php";
+    }
+    if (file_exists($filePath)) {
+        include $filePath;
+    } else {
+        $help = "<p>Not Found</p>";
+    }
+    $help = ob_get_contents();
+    ob_end_clean();
+} else {
+    $help = "<p>Not Found</p>";
+}
+
+$page[ 'body' ] .= "
+<div class=\"body_padded\">
+    {$help}
+</div>\n";
+
+dvwaHelpHtmlEcho( $page );
+```
+```
+
+Explanation:
+
+In the original code, the file path is constructed using user-controlled data (`$_GET[ 'id' ]` and `$_GET[ 'locale' ]`) without proper validation or sanitization. This can lead to directory traversal attacks and allow an attacker to access files outside the intended directory.
+
+To fix this, we should not construct the file path directly from user input. Instead, we can define the valid file paths explicitly and use the conditional logic to determine which file path to include based on the value of `$_GET[ 'locale' ]`.
+
+By using `file_exists()` to check if the file exists before including it, we can prevent the code from throwing warnings and errors if the file is not found.
+
+Included the file using `include` as it is safer than using `eval` since `include` only includes PHP code while `eval` can execute arbitrary code.
+
+Finally, we have added a fallback value for `$help` if the file is not found to prevent potential errors or vulnerabilities caused by uninitialized variables.
