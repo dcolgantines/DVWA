a5b2054f142fb98f05071bbaf3368159223c0f4b
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2023-12-18 10:01:25 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,114 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] = 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+To avoid constructing the path from user-controlled data, you can create a whitelist array of valid IDs and use the user input to index into the array. Here's an updated code snippet with the security vulnerability fixed:
+
+```php
+if (array_key_exists("id", $_GET)) {
+    $whitelist = array(
+        "javascript" => "JavaScript",
+        "fi" => "File Inclusion",
+        "brute" => "Brute Force",
+        "csrf" => "CSRF",
+        "exec" => "Command Injection",
+        "sqli" => "SQL Injection",
+        "sqli_blind" => "SQL Injection (Blind)",
+        "upload" => "File Upload",
+        "xss_r" => "Reflected XSS",
+        "xss_s" => "Stored XSS",
+        "weak_id" => "Weak Session IDs",
+        "authbypass" => "Authorisation Bypass"
+    );
+
+    $id = $_GET['id'];
+
+    if (array_key_exists($id, $whitelist)) {
+        $idPath = $whitelist[$id];
+
+        $lowsrc = @file_get_contents("./{$idPath}/source/low.php");
+        $lowsrc = str_replace(array('$html .='), array('echo'), $lowsrc);
+        $lowsrc = highlight_string($lowsrc, true);
+
+        $medsrc = @file_get_contents("./{$idPath}/source/medium.php");
+        $medsrc = str_replace(array('$html .='), array('echo'), $medsrc);
+        $medsrc = highlight_string($medsrc, true);
+
+        $highsrc = @file_get_contents("./{$idPath}/source/high.php");
+        $highsrc = str_replace(array('$html .='), array('echo'), $highsrc);
+        $highsrc = highlight_string($highsrc, true);
+
+        $impsrc = @file_get_contents("./{$idPath}/source/impossible.php");
+        $impsrc = str_replace(array('$html .='), array('echo'), $impsrc);
+        $impsrc = highlight_string($impsrc, true);
+
+        $vuln = $whitelist[$id];
+    } else {
+        // Invalid ID, handle accordingly
+        $vuln = 'Invalid ID';
+    }
+}
+```
+
+In this code, we create a whitelist array `$whitelist` that associates each valid ID with its corresponding vulnerability name. We then check if the user-provided ID exists in the `$whitelist` array before proceeding with the file operations. This ensures that only valid IDs are processed, preventing any path traversal or directory traversal attacks.
+		case "open_redirect" :
+			$vuln = 'Open HTTP Redirect';
+			break;
+		default:
+			$vuln = "Unknown Vulnerability";
+	}
+
+	$page[ 'body' ] .= "
+	<div class=\"body_padded\">
+		<h1>{$vuln}</h1>
+		<br />
+
+		<h3>Impossible {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$impsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>High {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$highsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Medium {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$medsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Low {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$lowsrc}</div></td>
+			</tr>
+		</table>
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"<-- Back\" onclick=\"history.go(-1);return true;\">
+		</form>
+
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho( $page );
+
+?>
