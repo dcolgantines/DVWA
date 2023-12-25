da82fd01c59cc9f5006555e797a46483971ac3f7
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2023-12-25 10:01:24 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,133 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] = 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+To avoid constructing the path from user-controlled data, you can use a whitelist of allowed values for the `$id` parameter and map it to the corresponding path. Here's an example of how you can rewrite the code:
+
+```php
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $allowedPaths = [
+        'javascript' => './javascript/source/',
+        'fi' => './fi/source/',
+        'brute' => './brute/source/',
+        'csrf' => './csrf/source/',
+        'exec' => './exec/source/',
+        'sqli' => './sqli/source/',
+        'sqli_blind' => './sqli_blind/source/',
+        'upload' => './upload/source/',
+        'xss_r' => './xss_r/source/',
+        'xss_s' => './xss_s/source/',
+        'weak_id' => './weak_id/source/',
+        'authbypass' => './authbypass/source/',
+    ];
+
+    if (array_key_exists($id, $allowedPaths)) {
+        $path = $allowedPaths[$id];
+
+        $lowsrc = @file_get_contents($path . 'low.php');
+        $lowsrc = str_replace(array('$html .='), array('echo'), $lowsrc);
+        $lowsrc = highlight_string($lowsrc, true);
+
+        $medsrc = @file_get_contents($path . 'medium.php');
+        $medsrc = str_replace(array('$html .='), array('echo'), $medsrc);
+        $medsrc = highlight_string($medsrc, true);
+
+        $highsrc = @file_get_contents($path . 'high.php');
+        $highsrc = str_replace(array('$html .='), array('echo'), $highsrc);
+        $highsrc = highlight_string($highsrc, true);
+
+        $impsrc = @file_get_contents($path . 'impossible.php');
+        $impsrc = str_replace(array('$html .='), array('echo'), $impsrc);
+        $impsrc = highlight_string($impsrc, true);
+
+        $vulns = [
+            'javascript' => 'JavaScript',
+            'fi' => 'File Inclusion',
+            'brute' => 'Brute Force',
+            'csrf' => 'CSRF',
+            'exec' => 'Command Injection',
+            'sqli' => 'SQL Injection',
+            'sqli_blind' => 'SQL Injection (Blind)',
+            'upload' => 'File Upload',
+            'xss_r' => 'Reflected XSS',
+            'xss_s' => 'Stored XSS',
+            'weak_id' => 'Weak Session IDs',
+            'authbypass' => 'Authorisation Bypass',
+        ];
+
+        $vuln = isset($vulns[$id]) ? $vulns[$id] : 'Unknown Vulnerability';
+
+        // Rest of the code
+
+    } else {
+        echo "Invalid 'id' parameter";
+    }
+}
+```
+
+In this code, we have created a whitelist array `$allowedPaths` that maps the allowed values of `$id` to their corresponding paths. The `$id` value is then used to retrieve the correct path from the whitelist, ensuring that only allowed paths are accessed.
+
+We have also added an additional check to handle the case when an invalid `$id` is provided. If the `$id` is not found in the whitelist, an error message is displayed.
+		case "open_redirect" :
+			$vuln = 'Open HTTP Redirect';
+			break;
+		default:
+			$vuln = "Unknown Vulnerability";
+	}
+
+	$page[ 'body' ] .= "
+	<div class=\"body_padded\">
+		<h1>{$vuln}</h1>
+		<br />
+
+		<h3>Impossible {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$impsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>High {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$highsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Medium {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$medsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Low {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$lowsrc}</div></td>
+			</tr>
+		</table>
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"<-- Back\" onclick=\"history.go(-1);return true;\">
+		</form>
+
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho( $page );
+
+?>
