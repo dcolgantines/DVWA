de22df06f83d16b885b6a43d5127e6e13bd594d5
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2024-01-01 10:01:10 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,123 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] .= 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+
+if (array_key_exists ("id", $_GET) && array_key_exists ("security", $_GET)) {
+	$id       = $_GET[ 'id' ];
+	$security = $_GET[ 'security' ];
+
+
+	switch ($id) {
+		case "fi" :
+			$vuln = 'File Inclusion';
+			break;
+		case "brute" :
+			$vuln = 'Brute Force';
+			break;
+		case "csrf" :
+			$vuln = 'CSRF';
+			break;
+		case "exec" :
+			$vuln = 'Command Injection';
+			break;
+		case "sqli" :
+			$vuln = 'SQL Injection';
+			break;
+		case "sqli_blind" :
+			$vuln = 'SQL Injection (Blind)';
+			break;
+		case "upload" :
+			$vuln = 'File Upload';
+			break;
+		case "xss_r" :
+			$vuln = 'Reflected XSS';
+			break;
+		case "xss_s" :
+			$vuln = 'Stored XSS';
+			break;
+		case "weak_id" :
+			$vuln = 'Weak Session IDs';
+			break;
+		case "javascript" :
+			$vuln = 'JavaScript';
+To patch the security vulnerability, you should use a whitelist approach instead of constructing the path from user-controlled data. Here is the updated code:
+
+```
+<?php
+// Whitelist of allowed vulnerabilities
+$allowedVulnerabilities = array(
+    'error_based_sqli',
+    'union_based_sqli',
+    'stored_xss',
+    'reflected_xss',
+    'authbypass',
+    'open_redirect',
+);
+
+$id = $_GET['id'];
+$security = $_GET['security'];
+
+// Check if the vulnerability is allowed
+if (!in_array($security, $allowedVulnerabilities)) {
+    $security = 'unknown';
+}
+
+$vuln = ucfirst(str_replace('_', ' ', $security));
+
+$validPath = DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.php";
+if (file_exists($validPath)) {
+    $source = @file_get_contents($validPath);
+    $source = str_replace(array('$html .='), array('echo'), $source);
+
+    $js_html = "";
+    $jsPath = DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.js";
+    if (file_exists($jsPath)) {
+        $js_source = @file_get_contents($jsPath);
+        $js_html = "
+        <h2>vulnerabilities/{$id}/source/{$security}.js</h2>
+        <div id=\"code\">
+            <table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+                <tr>
+                    <td><div id=\"code\">" . highlight_string($js_source, true) . "</div></td>
+                </tr>
+            </table>
+        </div>
+        ";
+    }
+
+    $page['body'] .= "
+    <div class=\"body_padded\">
+        <h1>{$vuln} Source</h1>
+
+        <h2>vulnerabilities/{$id}/source/{$security}.php</h2>
+        <div id=\"code\">
+            <table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+                <tr>
+                    <td><div id=\"code\">" . highlight_string($source, true) . "</div></td>
+                </tr>
+            </table>
+        </div>
+        {$js_html}
+        <br /><br />
+
+        <form>
+            <input type=\"button\" value=\"Compare All Levels\" onclick=\"window.location.href='view_source_all.php?id=$id'\">
+        </form>
+    </div>\n";
+} else {
+    $page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho($page);
+?>
+```
+
+In this updated code, we define a whitelist `$allowedVulnerabilities` that contains all the allowed vulnerabilities. We then check if the `$security` value is in the whitelist, and if not, we set it to `'unknown'`. This way, even if an attacker tries to manipulate the value of `$security` in the URL, it will be forced to a valid value.
+
+We also use the `$validPath` variable to construct the valid path to the source file, rather than directly concatenating user-controlled data in the `file_exists` and `file_get_contents` calls. This ensures that only valid paths within the allowed vulnerabilities are accessed.
