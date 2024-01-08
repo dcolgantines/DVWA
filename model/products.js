c463bc8c0591b82687226e12ed598ed98d721806
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2024-01-08 10:00:57 +0100

    OS commands should not be vulnerable to command injection attacks

---
+++ model/products.js
@@ -0,0 +1,24 @@
+<?php
+
+if( isset( $_POST[ 'Submit' ]  ) ) {
+	// Get input
+	$target = $_REQUEST[ 'ip' ];
+`
+
+	// Determine OS and execute the ping command.
+	if (stristr(php_uname('s'), 'Windows NT')) {
+		// Windows
+		$cmd = 'ping ' . escapeshellarg($target);
+	} else {
+		// *nix
+		$cmd = 'ping -c 4 ' . escapeshellarg($target);
+	}
+
+	// Execute the command and capture the output
+	$output = shell_exec($cmd);
+
+	// Feedback for the end user
+	$html .= "<pre>{$output}</pre>";
+}
+
+?>
