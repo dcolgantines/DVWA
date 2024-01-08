9a224cc80e7c77c7afe4d3b7907a9fe6a095a5f7
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2024-01-08 10:01:15 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,53 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+In order to avoid constructing the path from user-controlled data, we need to sanitize and validate the input provided in the `$_GET` array. You can do this by using the `filter_input()` function to retrieve the sanitized and validated input values. Here's the updated code:
+
+```php
+dvwaPageStartup(array('authenticated'));
+
+$page = dvwaPageNewGrab();
+$page['title'] = 'Help' . $page['title_separator'] . $page['title'];
+
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);
+$security = filter_input(INPUT_GET, 'security', FILTER_SANITIZE_STRING);
+$locale = filter_input(INPUT_GET, 'locale', FILTER_SANITIZE_STRING);
+
+if ($id !== null && $security !== null && $locale !== null) {
+    ob_start();
+    if ($locale == 'en') {
+        $helpFile = DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/help/help.php";
+    } else {
+        $helpFile = DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/help/help.{$locale}.php";
+    }
+    if (file_exists($helpFile)) {
+        include $helpFile;
+    } else {
+        echo 'Error: Help file not found';
+    }
+    $help = ob_get_contents();
+    ob_end_clean();
+} else {
+    $help = "<p>Not Found</p>";
+}
+
+$page['body'] .= "
+<div class=\"body_padded\">
+    {$help}
+</div>\n";
+
+dvwaHelpHtmlEcho($page);
+```
+
+In the updated code:
+1. The input values from `$_GET` array (`id`, `security`, and `locale`) are retrieved using `filter_input()` function with `FILTER_SANITIZE_STRING` filter option to sanitize the input values.
+2. If the input values are not null, the help file path is constructed based on the values of `id` and `locale` variables.
+3. The existence of the help file is checked using `file_exists()` function before including it to prevent arbitrary file inclusion.
+4. If the help file is not found, an error message is displayed.
+5. The contents of the help file are captured using `ob_start()` and `ob_get_contents()`.
+6. The captured contents are stored in the `$help` variable and the output buffering is cleaned using `ob_end_clean()`.
+7. Finally, the `$help` variable is added to the `$page['body']` and displayed as part of the page.
+
+Note: Make sure to replace `"FILTER_SANITIZE_STRING"` with the appropriate filter option based on your input requirements.
