41935978a5f6183333a3c6779fa91f54edecb4f6
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2024-01-08 10:01:07 +0100

    Database queries should not be vulnerable to injection attacks

---
+++ model/products.js
@@ -0,0 +1,38 @@
+<?php
+
+To fix the security vulnerability and avoid constructing SQL queries directly from user-controlled data, you need to use prepared statements. Here's the modified code:
+
+```php
+if (isset($_GET['Login'])) {
+    // Get username
+    $user = $_GET['username'];
+
+    // Get password
+    $pass = $_GET['password'];
+    $pass = md5($pass);
+
+    // Check the database
+    $query = "SELECT * FROM`users` WHERE user = ? AND password = ?";
+    $stmt = mysqli_prepare($GLOBALS["___mysqli_ston"], $query);
+    mysqli_stmt_bind_param($stmt, "ss", $user, $pass);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    if ($result && mysqli_num_rows($result) == 1) {
+        // Get user's details
+        $row = mysqli_fetch_assoc($result);
+        $avatar = $row["avatar"];
+
+        // Login successful
+        $html .= "<p>Welcome to the password protected area {$user}</p>";
+        $html .= "<img src=\"{$avatar}\" />";
+    } else {
+        // Login failed
+        $html .= "<pre><br />Username and/or password incorrect.</pre>";
+    }
+
+    ((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
+}
+```
+
+By using prepared statements, you ensure that user input is properly escaped and treated as data rather than part of the SQL query. This helps prevent SQL injection attacks.
