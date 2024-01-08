c2519fa8240e38d015f86e299c6dc593b73b40ad
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2024-01-08 10:01:19 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,116 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] = 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+To avoid constructing the path from user-controlled data, you can refactor the code to use a whitelist approach. Create an array to map the valid IDs to their corresponding vulnerabilities, and use the `$_GET['id']` value as a key to retrieve the vulnerability. Here's the modified code:
+
+```php
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    // Define the valid IDs and their corresponding vulnerabilities
+    $validIds = [
+        "javascript" => 'JavaScript',
+        "fi" => 'File Inclusion',
+        "brute" => 'Brute Force',
+        "csrf" => 'CSRF',
+        "exec" => 'Command Injection',
+        "sqli" => 'SQL Injection',
+        "sqli_blind" => 'SQL Injection (Blind)',
+        "upload" => 'File Upload',
+        "xss_r" => 'Reflected XSS',
+        "xss_s" => 'Stored XSS',
+        "weak_id" => 'Weak Session IDs',
+        "authbypass" => 'Authorisation Bypass'
+    ];
+
+    if (isset($validIds[$id])) {
+        $vuln = $validIds[$id];
+
+        $lowsrc = @file_get_contents("./{$id}/source/low.php");
+        $lowsrc = str_replace( array( '$html .=' ), array( 'echo' ), $lowsrc);
+        $lowsrc = highlight_string( $lowsrc, true );
+
+        $medsrc = @file_get_contents("./{$id}/source/medium.php");
+        $medsrc = str_replace( array( '$html .=' ), array( 'echo' ), $medsrc);
+        $medsrc = highlight_string( $medsrc, true );
+
+        $highsrc = @file_get_contents("./{$id}/source/high.php");
+        $highsrc = str_replace( array( '$html .=' ), array( 'echo' ), $highsrc);
+        $highsrc = highlight_string( $highsrc, true );
+
+        $impsrc = @file_get_contents("./{$id}/source/impossible.php");
+        $impsrc = str_replace( array( '$html .=' ), array( 'echo' ), $impsrc);
+        $impsrc = highlight_string( $impsrc, true );
+        
+        // Rest of the code...
+    } else {
+        // Handle invalid id value
+        // For example, redirect the user or show an error message
+        echo "Invalid ID";
+    }
+}
+```
+
+In the modified code, we check if the `$_GET['id']` value exists in the `$validIds` array. If it does, we retrieve the corresponding vulnerability and proceed with the rest of the code. If the `$_GET['id']` value is not in the whitelist, we can handle it accordingly (e.g., displaying an error message or redirecting the user). This approach helps prevent arbitrary file inclusion and ensures that only valid and expected values are used to construct the path.
+		case "open_redirect" :
+			$vuln = 'Open HTTP Redirect';
+			break;
+		default:
+			$vuln = "Unknown Vulnerability";
+	}
+
+	$page[ 'body' ] .= "
+	<div class=\"body_padded\">
+		<h1>{$vuln}</h1>
+		<br />
+
+		<h3>Impossible {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$impsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>High {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$highsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Medium {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$medsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Low {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$lowsrc}</div></td>
+			</tr>
+		</table>
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"<-- Back\" onclick=\"history.go(-1);return true;\">
+		</form>
+
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho( $page );
+
+?>
