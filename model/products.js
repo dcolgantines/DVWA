651c4645a29b6f39063d3c8ea9bc5cb1f95247b4
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2024-01-15 10:01:19 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,117 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] = 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+To avoid constructing the path from user-controlled data, you can create an array to map the "id" parameter to the corresponding file path and vulnerability name. Then, you can use the array to retrieve the correct file path and vulnerability name.
+
+Here's the updated code:
+
+```php
+$validIds = array(
+    "javascript" => array(
+        "path" => "./javascript/source",
+        "vulnerability" => "JavaScript"
+    ),
+    "fi" => array(
+        "path" => "./fi/source",
+        "vulnerability" => "File Inclusion"
+    ),
+    "brute" => array(
+        "path" => "./brute/source",
+        "vulnerability" => "Brute Force"
+    ),
+    // Add other valid ids and corresponding paths/vulnerabilities here
+);
+
+if (array_key_exists("id", $_GET)) {
+    $id = $_GET['id'];
+
+    if (array_key_exists($id, $validIds)) {
+        $idData = $validIds[$id];
+        $path = $idData['path'];
+        $vuln = $idData['vulnerability'];
+
+        $lowsrc = @file_get_contents("$path/low.php");
+        $lowsrc = str_replace(array('$html .='), array('echo'), $lowsrc);
+        $lowsrc = highlight_string($lowsrc, true);
+
+        $medsrc = @file_get_contents("$path/medium.php");
+        $medsrc = str_replace(array('$html .='), array('echo'), $medsrc);
+        $medsrc = highlight_string($medsrc, true);
+
+        $highsrc = @file_get_contents("$path/high.php");
+        $highsrc = str_replace(array('$html .='), array('echo'), $highsrc);
+        $highsrc = highlight_string($highsrc, true);
+
+        $impsrc = @file_get_contents("$path/impossible.php");
+        $impsrc = str_replace(array('$html .='), array('echo'), $impsrc);
+        $impsrc = highlight_string($impsrc, true);
+    } else {
+        // Handle invalid id case
+        // For example, display an error message or redirect to a default page
+    }
+}
+```
+
+In this updated code, we use the `$validIds` array to map each valid "id" parameter to the corresponding file path and vulnerability name. If the provided "id" is valid, we retrieve the path and vulnerability from the array and use them to construct the file paths for file_get_contents() calls. If the provided "id" is not valid, you can add code to handle the invalid case (e.g., displaying an error message or redirecting the user).
+		case "open_redirect" :
+			$vuln = 'Open HTTP Redirect';
+			break;
+		default:
+			$vuln = "Unknown Vulnerability";
+	}
+
+	$page[ 'body' ] .= "
+	<div class=\"body_padded\">
+		<h1>{$vuln}</h1>
+		<br />
+
+		<h3>Impossible {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$impsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>High {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$highsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Medium {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$medsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Low {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$lowsrc}</div></td>
+			</tr>
+		</table>
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"<-- Back\" onclick=\"history.go(-1);return true;\">
+		</form>
+
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho( $page );
+
+?>
