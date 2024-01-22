3ff4ef04fa0abbc434023e8f7e4ef26eec3cc170
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2024-01-22 10:01:24 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,108 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] = 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+
+if (array_key_exists ("id", $_GET)) {
+	$id = $_GET[ 'id' ];
+
+	$lowsrc = @file_get_contents("./{$id}/source/low.php");
+To avoid constructing the path from user-controlled data, you can use an associative array or a switch statement to map the user input to the corresponding file path. Here's an example using a switch statement:
+
+```php
+switch ($id) {
+    case "javascript":
+        $filePath = "javascript/source";
+        $vuln = 'JavaScript';
+        break;
+    case "fi":
+        $filePath = "fi/source";
+        $vuln = 'File Inclusion';
+        break;
+    case "brute":
+        $filePath = "brute/source";
+        $vuln = 'Brute Force';
+        break;
+    // Add more cases for other vulnerabilities
+
+    default:
+        $filePath = null; // Handle unknown vulnerability case
+        $vuln = "Unknown Vulnerability";
+}
+
+if ($filePath !== null) {
+    $lowsrc = file_get_contents("./{$filePath}/low.php");
+    $lowsrc = str_replace(array('$html .='), array('echo'), $lowsrc);
+    $lowsrc = highlight_string($lowsrc, true);
+
+    $medsrc = file_get_contents("./{$filePath}/medium.php");
+    $medsrc = str_replace(array('$html .='), array('echo'), $medsrc);
+    $medsrc = highlight_string($medsrc, true);
+
+    $highsrc = file_get_contents("./{$filePath}/high.php");
+    $highsrc = str_replace(array('$html .='), array('echo'), $highsrc);
+    $highsrc = highlight_string($highsrc, true);
+
+    $impsrc = file_get_contents("./{$filePath}/impossible.php");
+    $impsrc = str_replace(array('$html .='), array('echo'), $impsrc);
+    $impsrc = highlight_string($impsrc, true);
+}
+
+$page['body'] .= "
+<div class=\"body_padded\">
+    <h1>{$vuln}</h1>
+";
+```
+
+In this example, the `$filePath` variable is used to store the corresponding file path based on the user input (`$id`). If the user input matches a known vulnerability, the file paths are constructed using the `$filePath` variable. Otherwise, the `$filePath` variable remains `null` to handle the case of an unknown vulnerability.
+		<br />
+
+		<h3>Impossible {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$impsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>High {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$highsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Medium {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$medsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Low {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$lowsrc}</div></td>
+			</tr>
+		</table>
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"<-- Back\" onclick=\"history.go(-1);return true;\">
+		</form>
+
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho( $page );
+
+?>
