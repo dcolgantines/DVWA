5c818013b896fb71d7653e953332b224a0710295
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2024-01-22 10:01:08 +0100

    OS commands should not be vulnerable to command injection attacks

---
+++ model/products.js
@@ -0,0 +1,20 @@
+<?php
+
+if( isset( $_POST[ 'Submit' ]  ) ) {
+	// Get input
+	$target = $_REQUEST[ 'ip' ];
+```php
+// Determine OS and execute the ping command.
+if (stristr(php_uname('s'), 'Windows NT')) {
+    // Windows
+    $cmd = shell_exec('ping ' . escapeshellarg($target));
+} else {
+    // *nix
+    $cmd = shell_exec('ping -c 4 ' . escapeshellarg($target));
+}
+
+// Feedback for the end user
+$html .= "<pre>{$cmd}</pre>";
+```
+
+The code has been modified to use the `escapeshellarg()` function to escape the user-controlled data before constructing the command. This helps prevent command injection attacks by ensuring that the command argument passed to `shell_exec()` is properly formatted and sanitized.
