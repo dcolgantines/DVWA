806688f96eb9a644128664327baf084477fad03c
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2024-01-22 10:01:27 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,109 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] = 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+To fix the vulnerability of constructing the path from user-controlled data, you can create a whitelist array of allowed ids and then use the user input to directly retrieve the corresponding value from the whitelist array. Here's the updated code:
+
+```php
+$allowedIds = array(
+	"javascript" => 'JavaScript',
+	"fi" => 'File Inclusion',
+	"brute" => 'Brute Force',
+	"csrf" => 'CSRF',
+	"exec" => 'Command Injection',
+	"sqli" => 'SQL Injection',
+	"sqli_blind" => 'SQL Injection (Blind)',
+	"upload" => 'File Upload',
+	"xss_r" => 'Reflected XSS',
+	"xss_s" => 'Stored XSS',
+	"weak_id" => 'Weak Session IDs',
+	"authbypass" => 'Authorisation Bypass'
+);
+
+if (array_key_exists("id", $_GET)) {
+	$id = $_GET['id'];
+
+	if (array_key_exists($id, $allowedIds)) {
+		$vuln = $allowedIds[$id];
+
+		$lowsrc = @file_get_contents("./{$id}/source/low.php");
+		$lowsrc = str_replace(array('$html .='), array('echo'), $lowsrc);
+		$lowsrc = highlight_string($lowsrc, true);
+
+		$medsrc = @file_get_contents("./{$id}/source/medium.php");
+		$medsrc = str_replace(array('$html .='), array('echo'), $medsrc);
+		$medsrc = highlight_string($medsrc, true);
+
+		$highsrc = @file_get_contents("./{$id}/source/high.php");
+		$highsrc = str_replace(array('$html .='), array('echo'), $highsrc);
+		$highsrc = highlight_string($highsrc, true);
+
+		$impsrc = @file_get_contents("./{$id}/source/impossible.php");
+		$impsrc = str_replace(array('$html .='), array('echo'), $impsrc);
+		$impsrc = highlight_string($impsrc, true);
+	}
+}
+```
+
+In the updated code, we first define the `$allowedIds` array with the valid ids as keys and their corresponding values. Then, we check if the given `id` exists in the `$allowedIds` array before proceeding with further operations. This ensures that only the allowed ids will be used to construct the file paths.
+		case "open_redirect" :
+			$vuln = 'Open HTTP Redirect';
+			break;
+		default:
+			$vuln = "Unknown Vulnerability";
+	}
+
+	$page[ 'body' ] .= "
+	<div class=\"body_padded\">
+		<h1>{$vuln}</h1>
+		<br />
+
+		<h3>Impossible {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$impsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>High {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$highsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Medium {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$medsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Low {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$lowsrc}</div></td>
+			</tr>
+		</table>
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"<-- Back\" onclick=\"history.go(-1);return true;\">
+		</form>
+
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho( $page );
+
+?>
