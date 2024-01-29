4a192ae82c927850469aba143457db1442f14e95
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2024-01-29 10:01:22 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,127 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] = 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+
+if (array_key_exists ("id", $_GET)) {
+	$id = $_GET[ 'id' ];
+
+	$lowsrc = @file_get_contents("./{$id}/source/low.php");
+To avoid constructing the path from user-controlled data, you can use a whitelist approach. Create an array of allowed vulnerability IDs and use it to validate the user input. Here's the updated code:
+
+```php
+$allowedIds = [
+    "javascript",
+    "fi",
+    "brute",
+    "csrf",
+    "exec",
+    "sqli",
+    "sqli_blind",
+    "upload",
+    "xss_r",
+    "xss_s",
+    "weak_id",
+    "authbypass",
+    "open_redirect"
+];
+
+$id = isset($_GET['id']) ? $_GET['id'] : null;
+$vuln = '';
+
+if (in_array($id, $allowedIds)) {
+    switch ($id) {
+        case "javascript":
+            $vuln = 'JavaScript';
+            break;
+        case "fi":
+            $vuln = 'File Inclusion';
+            break;
+        // ... handle other cases ...
+        default:
+            $vuln = "Unknown Vulnerability";
+    }
+
+    $lowsrc = str_replace(array('$html .='), array('echo'), $lowsrc);
+    $lowsrc = highlight_string($lowsrc, true);
+
+    $medsrc = @file_get_contents("./" . $id . "/source/medium.php");
+    $medsrc = str_replace(array('$html .='), array('echo'), $medsrc);
+    $medsrc = highlight_string($medsrc, true);
+
+    $highsrc = @file_get_contents("./" . $id . "/source/high.php");
+    $highsrc = str_replace(array('$html .='), array('echo'), $highsrc);
+    $highsrc = highlight_string($highsrc, true);
+
+    $impsrc = @file_get_contents("./" . $id . "/source/impossible.php");
+    $impsrc = str_replace(array('$html .='), array('echo'), $impsrc);
+    $impsrc = highlight_string($impsrc, true);
+
+    $page['body'] .= "
+        <div class=\"body_padded\">
+            <h1>{$vuln}</h1>
+            // Rest of the code...
+        </div>";
+} else {
+    $vuln = "Invalid Vulnerability ID";
+    $page['body'] .= "
+        <div class=\"body_padded\">
+            <h1>{$vuln}</h1>
+        </div>";
+}
+```
+
+In this code, the `$allowedIds` array contains all the valid vulnerability IDs. If the user-provided `$id` exists in the whitelist, the code proceeds with processing the vulnerability and generating the appropriate response. If the `$id` is not in the whitelist, the code sets the `$vuln` variable to indicate an invalid vulnerability and generates the appropriate response.
+
+Please note that this code assumes the existence of a GET parameter named `id`. Make sure to adjust it according to your specific input handling.
+		<br />
+
+		<h3>Impossible {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$impsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>High {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$highsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Medium {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$medsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Low {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$lowsrc}</div></td>
+			</tr>
+		</table>
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"<-- Back\" onclick=\"history.go(-1);return true;\">
+		</form>
+
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho( $page );
+
+?>
