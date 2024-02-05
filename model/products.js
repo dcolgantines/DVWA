8e6b1f1d84aa513f40bd904e4177c050dcb7376f
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2024-02-05 10:01:02 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,48 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+```
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] = 'Help' . $page[ 'title_separator' ] . $page[ 'title' ];
+
+if (array_key_exists ("id", $_GET) &&
+    array_key_exists ("security", $_GET) &&
+    array_key_exists ("locale", $_GET)) {
+    $id       = $_GET[ 'id' ];
+    $security = $_GET[ 'security' ];
+    $locale = $_GET[ 'locale' ];
+
+    ob_start();
+    $helpPath = "";
+
+    if ($locale == 'en') {
+        $helpPath = DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/help/help.php";
+    } else {
+        $helpPath = DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/help/help.{$locale}.php";
+    }
+
+    if (file_exists($helpPath)) {
+        eval('?>' . file_get_contents($helpPath) . '<?php ');
+    } else {
+        echo "<p>Not Found</p>";
+    }
+
+    $help = ob_get_contents();
+    ob_end_clean();
+} else {
+    $help = "<p>Not Found</p>";
+}
+
+$page[ 'body' ] .= "
+<div class=\"body_padded\">
+    {$help}
+</div>\n";
+
+dvwaHelpHtmlEcho( $page );
+
+?>
+```
