e58f2610b7f712d8327564d94c6a994cf6bd8771
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2024-02-05 10:01:09 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,104 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] = 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+
+if (array_key_exists ("id", $_GET)) {
+	$id = $_GET[ 'id' ];
+
+	$lowsrc = @file_get_contents("./{$id}/source/low.php");
+To prevent constructing the path from user-controlled data, you can use a whitelist approach. Create an associative array to map the vulnerability IDs to their respective file paths. Then, retrieve the desired file path based on the given vulnerability ID and use that path directly.
+
+Here's the updated code:
+
+```php
+// Define the vulnerability IDs and their corresponding file paths
+$paths = array(
+    "javascript" => "./javascript/source.php",
+    "fi" => "./fi/source.php",
+    "brute" => "./brute/source.php",
+    "csrf" => "./csrf/source.php",
+    "exec" => "./exec/source.php",
+    "sqli" => "./sqli/source.php",
+    "sqli_blind" => "./sqli_blind/source.php",
+    "upload" => "./upload/source.php",
+    "xss_r" => "./xss_r/source.php",
+    "xss_s" => "./xss_s/source.php",
+    "weak_id" => "./weak_id/source.php",
+    "authbypass" => "./authbypass/source.php",
+    "open_redirect" => "./open_redirect/source.php",
+);
+
+// Retrieve the desired file path based on the vulnerability ID
+$filePath = isset($paths[$id]) ? $paths[$id] : "";
+
+// Read the file content and highlight it
+$src = "";
+if ($filePath && is_readable($filePath)) {
+    $src = file_get_contents($filePath);
+    $src = str_replace(array('$html .='), array('echo'), $src);
+    $src = highlight_string($src, true);
+}
+
+// Determine the vulnerability name
+$vuln = isset($paths[$id]) ? $paths[$id] : "Unknown Vulnerability";
+
+$page['body'] .= "
+<div class='body_padded'>
+    <h1>{$vuln}</h1>
+    {$src}
+</div>";
+```
+
+In this solution, the `$paths` array serves as a whitelist, mapping specific vulnerability IDs to their corresponding file paths. This ensures that only predefined file paths can be accessed, mitigating the risk of user-controlled paths.
+		<br />
+
+		<h3>Impossible {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$impsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>High {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$highsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Medium {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$medsrc}</div></td>
+			</tr>
+		</table>
+		<br />
+
+		<h3>Low {$vuln} Source</h3>
+		<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+			<tr>
+				<td><div id=\"code\">{$lowsrc}</div></td>
+			</tr>
+		</table>
+		<br /> <br />
+
+		<form>
+			<input type=\"button\" value=\"<-- Back\" onclick=\"history.go(-1);return true;\">
+		</form>
+
+	</div>\n";
+} else {
+	$page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho( $page );
+
+?>
