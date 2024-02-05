f2471c4ae0bd6a52637dc9fbd761d9ad5b8b55b5
dcolgantines <148070721+dcolgantines@users.noreply.github.com>
2024-02-05 10:01:20 +0100

    I/O function calls should not be vulnerable to path injection attacks

---
+++ model/products.js
@@ -0,0 +1,117 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ] .= 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+
+if (array_key_exists ("id", $_GET) && array_key_exists ("security", $_GET)) {
+	$id       = $_GET[ 'id' ];
+	$security = $_GET[ 'security' ];
+
+
+	switch ($id) {
+		case "fi" :
+			$vuln = 'File Inclusion';
+			break;
+		case "brute" :
+			$vuln = 'Brute Force';
+			break;
+		case "csrf" :
+			$vuln = 'CSRF';
+			break;
+		case "exec" :
+			$vuln = 'Command Injection';
+			break;
+		case "sqli" :
+			$vuln = 'SQL Injection';
+			break;
+		case "sqli_blind" :
+			$vuln = 'SQL Injection (Blind)';
+			break;
+		case "upload" :
+			$vuln = 'File Upload';
+			break;
+		case "xss_r" :
+			$vuln = 'Reflected XSS';
+			break;
+		case "xss_s" :
+			$vuln = 'Stored XSS';
+			break;
+		case "weak_id" :
+			$vuln = 'Weak Session IDs';
+			break;
+		case "javascript" :
+			$vuln = 'JavaScript';
+To patch the security vulnerability in the code, you can modify the code as follows:
+
+```
+switch ($security) {
+    case "file_inclusion" :
+        $vuln = 'File Inclusion';
+        break;
+    case "code_injection" :
+        $vuln = 'Code Injection';
+        break;
+    case "authbypass" :
+        $vuln = 'Authorisation Bypass';
+        break;
+    case "open_redirect" :
+        $vuln = 'Open HTTP Redirect';
+        break;
+    default:
+        $vuln = "Unknown Vulnerability";
+}
+
+$allowed_security = ["file_inclusion", "code_injection", "authbypass", "open_redirect"];
+if (in_array($security, $allowed_security)) {
+    $source_file_path = DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.php";
+    $source = @file_get_contents($source_file_path);
+    $source = str_replace(array('$html .='), array('echo'), $source);
+
+    $js_html = "";
+    $js_source_file_path = DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.js";
+    if (file_exists($js_source_file_path)) {
+        $js_source = @file_get_contents($js_source_file_path);
+        $js_html = "
+        <h2>vulnerabilities/{$id}/source/{$security}.js</h2>
+        <div id=\"code\">
+            <table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+                <tr>
+                    <td><div id=\"code\">" . highlight_string($js_source, true) . "</div></td>
+                </tr>
+            </table>
+        </div>
+        ";
+    }
+
+    $page['body'] .= "
+    <div class=\"body_padded\">
+        <h1>{$vuln} Source</h1>
+
+        <h2>{$source_file_path}</h2>
+        <div id=\"code\">
+            <table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
+                <tr>
+                    <td><div id=\"code\">" . highlight_string($source, true) . "</div></td>
+                </tr>
+            </table>
+        </div>
+        {$js_html}
+        <br /> <br />
+
+        <form>
+            <input type=\"button\" value=\"Compare All Levels\" onclick=\"window.location.href='view_source_all.php?id=$id'\">
+        </form>
+    </div>\n";
+} else {
+    $page['body'] = "<p>Not found</p>";
+}
+
+dvwaSourceHtmlEcho($page);
+```
+
+In the patched code, I've added an `$allowed_security` array that contains the list of allowed values for `$security`. Before constructing the file paths, we check if `$security` is included in the `$allowed_security` array. This ensures that no user-controlled data is used to construct the file paths, preventing any potential path traversal attacks.
